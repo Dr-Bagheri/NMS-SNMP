eb86bf345b98b2f86413672e6389ebd2a4beba72
Amirreza Bagheri <80455495+Dr-Bagheri@users.noreply.github.com>
2024-05-10 20:14:00 +0200

    Update models.py

--- network_monitoring_backend/network_data/models.py
+++ network_monitoring_backend/network_data/models.py
@@ -1,18 +1,18 @@
-from timescale.db.models import Model
-from timescale.db.models.fields import TimescaleDateTimeField, FloatField, CharField, TextField, IntegerField
+from django.db import models
+from timescale.db.models.fields import TimescaleDateTimeField
 
-class DeviceData(Model):
-    timestamp = TimescaleDateTimeField(auto_now_add=True)
-    device_name = CharField(max_length=255)
-    cpu_usage = FloatField()
-    ram_usage = FloatField()
-    device_ip = CharField(max_length=255)  
-    device_id = IntegerField()  
+class DeviceData(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)  # Automatically set the timestamp on record creation
+    device_name = models.CharField(max_length=255)
+    cpu_usage = models.FloatField()
+    ram_usage = models.FloatField()
+    device_ip = models.CharField(max_length=255)  
+    device_id = models.IntegerField()  
     class Meta:
         # This orders the data by 'timestamp' descending, so newest entries first
         ordering = ['-timestamp']
 
-class Post(Model):
+class Post(models.Model):
     
-    title = CharField(max_length=255)
-    content = TextField()
+    title = models.CharField(max_length=255)
+    content = models.TextField()
