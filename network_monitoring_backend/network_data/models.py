4576e36a41cc6ffbea1b8cb7156b3cffadd0d922
Amirreza Bagheri <80455495+Dr-Bagheri@users.noreply.github.com>
2024-05-12 11:17:15 +0200

    update

--- network_monitoring_backend/network_data/models.py
+++ network_monitoring_backend/network_data/models.py
@@ -6,7 +6,8 @@ class DeviceData(models.Model):
     device_name = models.CharField(max_length=255)
     cpu_usage = models.FloatField()
     ram_usage = models.FloatField()
-
+    #device_ip = models.BigIntegerField()  
+    #device_id = models.IntegerField()  
     class Meta:
         # This orders the data by 'timestamp' descending, so newest entries first
         ordering = ['-timestamp']
